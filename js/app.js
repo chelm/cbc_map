5bc172f12d0212fd38dfb03b24d0dc9fbe6ffa92
chelm <christopher_helm@esri.com>
2013-05-07 07:45:25 -0600

    barely working

--- js/app.js
+++ js/app.js
@@ -4,12 +4,16 @@ function App( path ){
     funders: [],
     years: [],
     types: [],
-    counties: []
+    counties: [],
+    county_agg: {}
   };
 
   function init(){
     _app.vis = d3.select('#vis');
     _app.cat20 = d3.scale.category20();
+    _app.orange = d3.scale.quantile()
+      .domain([0, 275])
+      .range(["#feedde","#fdd0a2","#fdae6b","#fd8d3c","#f16913","#d94801","#8c2d04"])
 
     _app.data = {};
 
@@ -17,7 +21,29 @@ function App( path ){
       if (_app.years.indexOf(+d.year) == -1) _app.years.push(+d.year);
       if (_app.funders.indexOf(d.funder) == -1) _app.funders.push(d.funder);
       if (_app.types.indexOf(d.type) == -1) _app.types.push(d.type);
-      if (_app.counties.indexOf(d.county.toLowerCase()) == -1) _app.counties.push(d.county.toLowerCase());
+      
+      var county = d.county.toLowerCase();
+      if (_app.counties.indexOf(county) == -1) _app.counties.push(county);
+      //if (_app.counties.indexOf(county) == -1 && d.region == 'WesternSlope') _app.counties.push(county);
+
+      if ( !_app.county_agg[ county ]) _app.county_agg[county] = {
+        money: 0,
+        grants: 0,
+        grant_years: {
+          2009: 0, 
+          2010: 0,
+          2011: 0,
+          2012: 0
+        },
+        nonprofits: []
+      };
+
+      _app.county_agg[ county ].money += +d.amount;
+      _app.county_agg[ county ].grants++;
+      _app.county_agg[ county ].grant_years[+d.year]++;
+      if (_app.county_agg[ county ].nonprofits.indexOf( d.nonprofit ) == -1) {
+        _app.county_agg[ county ].nonprofits.push( d.nonprofit );
+      }
 
       var g = {
         year: +d.year,
@@ -42,7 +68,7 @@ function App( path ){
       _app.grants = rows;
       _app.selected = 'all';
       
-      initChart();
+      _app.chart();
       map();
       controls();
     });
@@ -53,6 +79,7 @@ function App( path ){
     var projection = d3.geo.mercator()
       .center([-100, 37.5])
       .scale(3000);
+
     var path = d3.geo.path().projection(projection);
 
     d3.json("data/co_counties.json", function( data ){
@@ -68,16 +95,15 @@ function App( path ){
         .enter().append("path")
           .attr("d", path)
           .attr("class", "county")
-          .style('fill', function(d){ if (_app.counties.indexOf(d.properties.COUNTY.toLowerCase()) != -1) return _app.cat20(d.properties.COUNTY); })
+          .style('fill', function(d){ if (_app.counties.indexOf(d.properties.COUNTY.toLowerCase()) != -1) return _app.orange(_app.county_agg[d.properties.COUNTY.toLowerCase()].grants) })
           .attr("id", function(d){
-            //console.log('county', d);
             return d.properties.COUNTY;
           })
           .on('mouseover', function(){
-            _app.showCounty( this.id.toLowerCase() );
+            if (_app.counties.indexOf(this.id.toLowerCase()) != -1) _app.showCounty( this.id.toLowerCase() );
           })
           .on('mouseout', function(){
-            d3.select('#county_info').style('display', 'none');        
+            //d3.select('#county_info').style('display', 'none');        
           }) 
     })
   }
@@ -113,15 +139,6 @@ function App( path ){
           _app.chart();
         });
 
-    /*control_div.append('div')
-      .attr('id', 'years')
-      //.text('Years')
-      .selectAll('div')
-      .data(_app.years)
-      .enter().append('div')
-        .attr('class', 'year')
-        .text(function(d){ return d});*/
-  
   }
 
   _app.chart = function(){
@@ -130,41 +147,42 @@ function App( path ){
     if ( funders != 'all' ) {
       d3.selectAll('.funder').style('background', '#aaa');
       funders.forEach(function(f){
-        console.log(f, d3.select('#'+f.replace(/ /g, '_')));
+        //console.log(f, d3.select('#'+f.replace(/ /g, '_')));
         d3.select('#'+f.replace(/ /g, '_')).style('background', function(d){ return _app.cat20(f);})
       });
     }
 
     // build charting data
     var chart_data = {};
+    var totals = {};
     for ( var yr in _app.data ){
       if ( !chart_data[yr] ) chart_data[yr] = {};
+      if ( !totals[yr] ) totals[yr] = 0;
       for ( var funder in _app.data[yr] ){
         var index = funders.indexOf(funder);
         if ( index >= 0 ){
           chart_data[ yr ][ funder ] = _app.data[ yr ][ funder ].length;
+          totals[yr] += _app.data[ yr ][ funder ].length;
         }
       }
     }
 
-    console.log(chart_data)
-
-    
-
+    d3.select("#chart svg").remove();
+    drawChart( totals );
 
   }
 
-  initChart = function(){
+  drawChart = function( totals ){
 
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
       width = 660 - margin.left - margin.right,
-      height = 150 - margin.top - margin.bottom;
+      height = 200 - margin.top - margin.bottom;
 
     var x = d3.scale.ordinal()
         .rangeRoundBands([0, width], .1);
     
-    var y = d3.scale.linear()
-        .rangeRound([height, 0]);
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([height, 0]);
     
     var color = d3.scale.ordinal()
         .domain(_app.funders)
@@ -185,22 +203,14 @@ function App( path ){
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-   /* data.forEach(function(d) {
-      var y0 = 0;
-      d.ages = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
-      d.total = d.ages[d.ages.length - 1].y1;
-    });*/
-  
-    //data.sort(function(a, b) { return b.total - a.total; });
-  
     x.domain(_app.years);
-    y.domain([0, 25]);
-  
+    y.domain([0, d3.max(Object.keys(totals), function(d) { return totals[d]; })]);
+
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
-  
+
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
@@ -209,63 +219,18 @@ function App( path ){
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Population");
-  
-    /*var state = svg.selectAll(".state")
-        .data(data)
-      .enter().append("g")
-        .attr("class", "g")
-        .attr("transform", function(d) { return "translate(" + x(d.State) + ",0)"; });
-  
-    state.selectAll("rect")
-        .data(function(d) { return d.ages; })
-      .enter().append("rect")
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d.y1); })
-        .attr("height", function(d) { return y(d.y0) - y(d.y1); })
-        .style("fill", function(d) { return color(d.name); });*/
+        .text("grants");
 
-
-   /* var n = 4, // number of layers
-    m = 58, // number of samples per layer
-    stack = d3.layout.stack(),
-    layers = stack(d3.range(n).map(function() { return bumpLayer(m, .1); })),
-    yGroupMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y; }); }),
-    yStackMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
-
-    var x = d3.scale.ordinal()
-      .domain([2009, 2010, 2011, 2012])
-      .rangeRoundBands([0, 600], .08);
-
-    var y = d3.scale.linear()
-      .domain([0, 1])
-      .range([100, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+    svg.selectAll(".bar")
+      .data(Object.keys(totals))
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(totals[d]); })
+      .attr("height", function(d) { return height - y(totals[d]); });
     
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
 
-    var vis = d3.select("#chart").append("svg")
-        .attr("width",700)
-        .attr("height",200);  
-
-    vis.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + 100 + ")")
-      .call(xAxis);
-
-    vis.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-    var layer = vis.selectAll(".layer")
-      .data( layers )
-    .enter().append("g")
-      .attr( "class", "layer" )
-      .style( "fill", function(d, i) { ;return '#08c'; });*/
 
   }
 
@@ -273,8 +238,61 @@ function App( path ){
   _app.showCounty = function( name ){
     var el = d3.select('#county_info');
     el.style('display', 'block');
+    var data = _app.county_agg[name];
 
     d3.select('#county_name').text(name + ' County')
+    d3.select('#county_grants').html('<td class="stat">'+ data.grants +'</td> <td> grants awarded </td>');
+    d3.select('#county_money').html('<td class="stat">$'+ Math.round(data.money).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +'</td><td> in donations</td>');
+    d3.select('#county_nonprofits').html('<td class="stat">'+ data.nonprofits.length +'</td> <td> organizations </td>');
+
+    d3.select('#county_chart').select('svg').remove();
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 300 - margin.left - margin.right,
+      height = 150 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+
+    
+    var svg = d3.select('#county_chart').append('svg')
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(_app.years);
+    y.domain([0, d3.max(Object.keys(data.grant_years), function(d) { return data.grant_years[d]; })]);
+
+     svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+
+    svg.selectAll(".bar")
+      .data(Object.keys(data.grant_years))
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(data.grant_years[d]); })
+      .attr("height", function(d) { return height - y(data.grant_years[d]); });
+
 
   }
 
