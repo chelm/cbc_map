1cbe104b6e9dc3b714012a8c95f7acc819878be0
chelm <christopher.helm@gmail.com>
2015-08-01 20:49:35 -0600

    adding the data work

---
+++ 2015-data-work/create-all-regions.sh
@@ -0,0 +1,8 @@
+node index.js Northeast > ../2015/data/Northeast.csv
+node index.js Northwest > ../2015/data/Northwest.csv
+node index.js Southwest > ../2015/data/Southwest.csv
+node index.js Southeast > ../2015/data/Southeast.csv
+node index.js San\ Juan > ../2015/data/San\ Juan.csv
+node index.js San\ Luis\ Valley > ../2015/San\ Luis\ Valley.csv
+node index.js Heart\ of\ Colorado > ../2015/data/Heart\ of\ Colorado.csv
+node index.js Western\ Slope > ../2015/data/Western\ Slope.csv
